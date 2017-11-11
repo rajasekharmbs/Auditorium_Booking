d5902d4264c3217fa53f0a56c88be6673819a41e
rajasekharmbs <33427838+rajasekharmbs@users.noreply.github.com>
2017-11-12 00:19:04 +0530

    Update login.php
    Implemented database

--- login.php
+++ login.php
@@ -1,3 +1,4 @@
+
 <html>
 	<head>
 		<title>Log in</title>
@@ -10,22 +11,90 @@
   		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	</head>
 	<body>
+
+			<nav class="navbar navbar-custom">
+  <div class="container-fluid">
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="index.php">Home</a></li>
+    </ul>
+    <form class="navbar-form navbar-right">
+      <div class="form-group">
+        <a href="signup.php" class="btn">
+          <span class="glyphicon glyphicon-user"></span> Sign up 
+        </a>
+      </div>
+    </form>
+  </div>
+</nav>
+
+
+
 	<div class="container">
 	<div class="jumbotron">
-		<form>
+		<form method="post">
 		  <div class="form-group">
+		  <span class="glyphicon glyphicon-envelope"></span>
 		    <label for="exampleInputEmail1">Email address</label>
-		    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+		    <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
 		    
 		  </div>
 		  <div class="form-group">
+		  <span class="glyphicon glyphicon-lock"></span>
 		    <label for="exampleInputPassword1">Password</label>
-		    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+		    <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
 		  </div>
-		  <center><button type="submit" class="btn btn-primary">Log in</button></center>
+		  <center><button type="submit" name="submit" class="btn btn-primary">Log in</button></center>
 		</form>	
 	</div>
 	</div>
 		
 	</body>
 </html>
+
+
+
+<?php
+if($_POST){
+	if(isset($_POST['submit'])){
+	if(!empty($_POST['email']) and !empty($_POST['password'])){
+
+		$email = $_POST['email'];
+		$password = $_POST['password'];
+
+		$host = "localhost";
+		$user = "root";
+		$pass = "";
+		$dbname = "project";
+
+		$connection = mysqli_connect($host,$user,$pass,$dbname);
+
+		if($connection){
+
+			$query = "select email,pass from user where email='$email'";
+			$result = mysqli_query($connection,$query);
+			#printf("lines : %d\n", mysqli_affected_rows($connection));
+			if($result){
+
+				$row = mysqli_fetch_assoc($result);
+
+				if($row['email']==$email and $row['pass']==$password){
+
+					header("Location:main.php");
+
+				}
+
+			}
+
+			mysqli_close($connection);
+
+		}
+		else{
+			echo "connection not established";
+		}
+
+	}
+}
+}
+
+
+?>
